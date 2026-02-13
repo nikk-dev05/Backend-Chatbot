77c77289c6ad782a7da540edadb63b7efd3405cf
nikk-dev05 <nikhil98627@gmail.com>
2026-02-13 18:49:09 +0530

    fixed cors issues

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5500',
+    origin: process.env.FRONTEND_URL || "https://frontend-chatbot-blush.vercel.app",
     credentials: true,
 }));
 
