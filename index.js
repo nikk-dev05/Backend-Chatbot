2261363067d4874e99bdfc9242181c1438789131
nikk-dev05 <nikhil98627@gmail.com>
2026-02-13 19:10:01 +0530

    again updated code

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin:"https://frontend-chatbot-blush.vercel.app",
-    credentials: true,
+  origin: "https://frontend-chatbot-blush.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
+
+app.options('*', cors());
 
 app.use(express.json());
 const connectDB = async () => {
